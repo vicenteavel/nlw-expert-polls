59891911f0ad789e0a82008b23528808aa687645
Vicente Avelino <vicente@vsdi.com.br>
2024-02-08 10:07:27 -0300

    feat: add pubsub pattern in voting on poll to websocket

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -4,6 +4,7 @@ import { randomUUID } from "node:crypto";
 
 import { prisma } from "../../lib/prisma";
 import { redis } from "../../lib/redis";
+import { voting } from "../utils/voting-pub-sub";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -40,7 +41,16 @@ export async function voteOnPoll(app: FastifyInstance) {
           },
         });
 
-        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId);
+        const votes = await redis.zincrby(
+          pollId,
+          -1,
+          userPreviousVoteOnPoll.pollOptionId
+        );
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+          votes: Number(votes),
+        });
       } else if (userPreviousVoteOnPoll) {
         return reply
           .status(400)
@@ -67,7 +77,12 @@ export async function voteOnPoll(app: FastifyInstance) {
       },
     });
 
-    await redis.zincrby(pollId, 1, pollOptionId);
+    const votes = await redis.zincrby(pollId, 1, pollOptionId);
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes),
+    });
 
     return reply.status(201).send();
   });
